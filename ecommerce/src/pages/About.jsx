2a5811e9effe184dee420b0b08535132f918d994
MarinaaMaged <marina.maged200@gmail.com>
2025-01-24 15:36:01 +0200

    About, contact us, order and login page

--- ecommerce/src/pages/About.jsx
+++ ecommerce/src/pages/About.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import { assets } from '../assets/frontend_assets/assets'
+import Title from '../components/Title';
 
 const About=()=>{
   return(
     <div>
+        <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT '} text2={'US'}/>
 
+        </div>
+        <div className='my-10 flex flex-col md:flex-row gap-4'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Shopify is your blend of fashion, trends and everything you can ever want. Our platform is where customers can easily discover, explore and purchase wide range of products from the comfort </p>
+          <p>Enjoy your shopping with us! We are here to provide you with the ultimate experience of fashion!</p>
+            <b className='text-gray-800'>Our Mission</b>
+            <p>Our mission at Shopify is to empower customers with elegance, choice and everything they admire</p>
+        </div>
+        </div>
+        <div className='text-2xl py-4'>
+          <Title text1={'Why '} text2={'Shopify?'}/>
+          <div className=' border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+              <p> we select and ensure that each product meets the best in the town</p>
+          </div>
+
+        </div>        
     </div>
+    
   )
 }
 
-export default About
+export default About 
--- ecommerce/src/pages/PlaceOrder.jsx
+++ ecommerce/src/pages/PlaceOrder.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { shopContext } from '../context/ShopContextProvider'
 
 const PlaceOrder=()=>{
+
+  const {navigate}=useContext(shopContext);
   return(
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
@@ -29,7 +32,7 @@ const PlaceOrder=()=>{
 
         </div>
         <div className='w-full text-end mt-8'>
-          <button className='bg-black text-white px-16 py-3 text-sm'> Place Order</button>
+          <button onClick={()=>navigate('/orders')} className='bg-pink-300 text-white px-16 py-3 text-sm'> Place Order</button>
 
         </div>
 
