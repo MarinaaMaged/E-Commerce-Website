1399acac6fdc2e4655a66eca319d19dc61e03e3f
MarinaaMaged <marina.maged200@gmail.com>
2025-01-24 16:54:10 +0200

    editing some files #3

--- ecommerce/src/pages/Home.jsx
+++ ecommerce/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import LatestCollection from '../components/latestCollection'
+import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import Policy from '../components/Policy'
 const Home=()=>{
